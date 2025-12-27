f5816ba62c5f51da0857768ec9fa1ebf8d195c26
jewelsinthepalace2-create <jewelsinthepalace2@gmail.com>
2025-12-28 07:35:40 +0800

    Update lists.js

--- js/lists.js
+++ js/lists.js
@@ -1,22 +1,12 @@
-fetchData("/members");
-
-function fetchData(endpoint) {
-  fetch(API_URL + endpoint)
-    .then(res => res.json())
-    .then(data => {
-      const list = document.getElementById("list");
-
-      data.forEach(user => {
-        const div = document.createElement("div");
-        div.className = "card";
-        div.innerHTML = `
-          <img src="../assets/avatar.png">
-          <strong>${user.name}</strong>
-        `;
-        div.onclick = () => {
-          window.location.href = `../profile/profile.html?id=${user.id}`;
-        };
-        list.appendChild(div);
-      });
+fetch(API_URL + "/members")
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById("list");
+    data.forEach(u => {
+      const d = document.createElement("div");
+      d.className = "card";
+      d.innerHTML = `<img src="../assets/avatar.png"><strong>${u.name}</strong>`;
+      d.onclick = () => location.href = `../profile/profile.html?id=${u.id}`;
+      list.appendChild(d);
     });
-}
+  });
